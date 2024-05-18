116cb4f3372a79acf2199a21d33161dec66a4590
harapriya mohanty <harapriyamohanty270@gmail.com>
2024-05-18 07:41:54 +0530

    check perfect number or not

---
+++ perfect.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int i,n,s=0;
+printf("Enter the number \n");
+scanf("%d",&n);
+for (i=1;i<n;i++)
+{
+if(n%i==0)
+{
+s=s+i;
+}
+}
+if(s==n)
+{
+printf("perfect number\n ");
+}
+else
+{
+printf("not perfect number \n ");
+}
+}
